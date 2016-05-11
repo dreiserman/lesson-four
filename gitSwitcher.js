98401769196bb973d6922c586f134db4e936d224
Jonathan DeAscentis <jonathandeascentis@Jonathans-MacBook-Pro-2.local>
2016-05-10 18:51:22 -0700

    First commit

---
+++ gitSwitcher.js
@@ -0,0 +1,15 @@
+var weekday = prompt('Cough up a day of the week.')
+    
+    switch(weekday) {
+      case 'Monday': console.log("It's jsut another manic monday.");
+      break:
+      case 'Tuesday': console.log('Goodbye, Ruby Tuesday.');
+      break:
+      case 'Wednesday': console.log('And tonight will be all I have to recall.');
+      break:
+      case 'Thursday': console.log("Only for you I don't regret that I was Thursday's child");
+      break;
+      case 'Friday': console.log('Friday night and the lights are low.');
+      break:
+      default: console.log('Try again. You must be daydreaming.');
+    }
